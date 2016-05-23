a748456fbbea0bdf5c4f11ed8933b1b6687670d2
Flávio Prado <flavio_roberto_prado@hotmail.com>
2016-05-23 09:49:45 -0300

    Update Exercicio51.java

--- Exercicio51.java
+++ Exercicio51.java
@@ -359,7 +359,7 @@ public class Exercicio51 {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (cod <= 2) {
+				if (cod <= 100) {
 					salvarRegistro();
 					campo4.setText("");
 					campo4.setText("" + cod);
